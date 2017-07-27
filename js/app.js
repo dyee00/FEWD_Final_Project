d8e8478a293c00cea9fa5d812760ec1a547cd6b2
dyee00 <dyee00@gmail.com>
2017-07-27 17:53:59 -0400

    10

--- js/app.js
+++ js/app.js
@@ -40,6 +40,55 @@ $(".modal-close").click(function(event) {
 	$("#modal-cafe-latte").hide("#modal-show");
 });
 
+$(".flat-white").click(function() { 	
+	$("#modal-flat-white").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-flat-white").hide("#modal-show");
+});
+
+$(".americano").click(function() { 	
+	$("#modal-americano").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-americano").hide("#modal-show");
+});
+
+$(".red-eye").click(function() { 	
+	$("#modal-red-eye").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-red-eye").hide("#modal-show");
+});
+
+$(".macchiato").click(function() { 	
+	$("#modal-macchiato").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-macchiato").hide("#modal-show");
+});
+
+$(".cortado").click(function() { 	
+	$("#modal-cortado").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-cortado").hide("#modal-show");
+});
+
+$(".mocha").click(function() { 	
+	$("#modal-mocha").show("#modal-show");
+});
+
+$(".modal-close").click(function(event) {
+	$("#modal-mocha").hide("#modal-show");
+});
+
+
 
 
 /* closes modal on background click
