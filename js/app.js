bb1f98c32902e5e0b1c020a2b7e54788e55ca094
dyee00 <dyee00@gmail.com>
2017-07-30 01:12:42 -0400

    7/30/17 - 1:12am

--- js/app.js
+++ js/app.js
@@ -7,29 +7,28 @@ $(".espresso").click(function() {
 	$("#modal-espresso").show("#modal-show");
 });
 
-
 $(".modal-close").click(function(event) {
 	$("#modal-espresso").hide("#modal-show");
 });
- 
+
 
 $(".cappuccino").click(function() { 	
 	$("#modal-cappuccino").show("#modal-show");
 });
 
-
 $(".modal-close").click(function(event) {
 	$("#modal-cappuccino").hide("#modal-show");
 });
+
 
 $(".cafe-latte").click(function() { 	
 	$("#modal-cafe-latte").show("#modal-show");
 });
 
-
 $(".modal-close").click(function(event) {
 	$("#modal-cafe-latte").hide("#modal-show");
 });
+
 
 $(".flat-white").click(function() { 	
 	$("#modal-flat-white").show("#modal-show");
@@ -39,6 +38,7 @@ $(".modal-close").click(function(event) {
 	$("#modal-flat-white").hide("#modal-show");
 });
 
+
 $(".americano").click(function() { 	
 	$("#modal-americano").show("#modal-show");
 });
@@ -46,6 +46,7 @@ $(".americano").click(function() {
 $(".modal-close").click(function(event) {
 	$("#modal-americano").hide("#modal-show");
 });
+
 
 $(".red-eye").click(function() { 	
 	$("#modal-red-eye").show("#modal-show");
@@ -55,6 +56,7 @@ $(".modal-close").click(function(event) {
 	$("#modal-red-eye").hide("#modal-show");
 });
 
+
 $(".macchiato").click(function() { 	
 	$("#modal-macchiato").show("#modal-show");
 });
@@ -63,6 +65,7 @@ $(".modal-close").click(function(event) {
 	$("#modal-macchiato").hide("#modal-show");
 });
 
+
 $(".cortado").click(function() { 	
 	$("#modal-cortado").show("#modal-show");
 });
@@ -70,6 +73,7 @@ $(".cortado").click(function() {
 $(".modal-close").click(function(event) {
 	$("#modal-cortado").hide("#modal-show");
 });
+
 
 $(".mocha").click(function() { 	
 	$("#modal-mocha").show("#modal-show");
@@ -83,7 +87,7 @@ $(".modal-close").click(function(event) {
 // ==== Button - pervious/next ====
 
 var pageIndex = 1;
-hidePages(pageIndex);
+// hidePages(pageIndex);
 
 function plusSlides(n) {
   showPages(pageIndex += n);
@@ -106,12 +110,18 @@ function showPages(n) {
 
 /* closes modal on background click 
 
-$("#modal-espresso").click(function(event) {
-  	event.preventDefault();
+$("#modal-container").click (function(event) {
+ 	event.preventDefault();
 
-    $("#modal-espresso").hide();
-   
+    $("#modal-container").hide("modal-content");
+    
+
 });
 
 */
+
+
+
+
+
 
